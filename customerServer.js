28b0d25cceececb698ff2dd7d9ac0a86e7168853
sujeet_1miles <sujeet74560@gmail.com>
2023-09-18 12:10:33 +0530

    customer1

--- customerServer.js
+++ customerServer.js
@@ -1,11 +1,8 @@
 let express=require("express");
 let cors = require("cors");
 let app = express();
-app.use(cors({
-    origin:"http://localhost:3000",
-    methods:["GET","POST","PUT","DELETE"]
-}));
 app.use(express.json());
+app.use(cors());
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin","*");
     res.header(
